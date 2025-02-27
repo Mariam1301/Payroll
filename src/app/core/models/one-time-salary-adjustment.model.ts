db1f06aa4aef9bff736c431297c443beb7dab0c7
mariamkhachvani <marikhachvani13@gmail.com>
2025-02-27 20:03:23 +0400

    fix integration issues

--- src/app/core/models/one-time-salary-adjustment.model.ts
+++ src/app/core/models/one-time-salary-adjustment.model.ts
@@ -3,8 +3,10 @@ import { AdjustmentTypeEnum, CurrencyEnum } from './general.model';
 export interface OneTimeSalaryAdjustment {
   id: number;
   employee_id: number;
-  adjustment_type: AdjustmentTypeEnum;
+  type: AdjustmentTypeEnum;
   amount: number;
+  employee_name: string;
+  employee_surname: string;
   calculation_currency: CurrencyEnum;
   includes_income_tax: boolean;
   includes_employee_pension: boolean;
